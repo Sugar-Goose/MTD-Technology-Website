b1076e815795ac2c1d5491877a354497a4028723
Sugar Goose <130372150+Sugar-Goose@users.noreply.github.com>
2025-04-10 14:52:17 +0300

    Added new header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainComp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComp from './components/MainComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainComp
   }
 }
 </script>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mainContainer">
+    <h1>Development Started...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
